787a255356dcc9f92d7b7a0a44594d17476cd111
Jakob Schechter <jakeeli52@gmail.com>
2023-03-02 13:31:41 -0500

    Separated handling keyboard events from UserScoreInput - Opens up possibility of different input types in the future

--- src/jvmMain/kotlin/com/tainin/composablepegboard/Main.kt
+++ src/jvmMain/kotlin/com/tainin/composablepegboard/Main.kt
@@ -28,6 +28,7 @@ import com.tainin.composablepegboard.model.UserScoreInput
 import com.tainin.composablepegboard.pegboard.boards.RectangularSpiralBoard
 import com.tainin.composablepegboard.pegboard.options.StreetOptions
 import com.tainin.composablepegboard.pegboard.overlays.PegsOverlay
+import com.tainin.composablepegboard.utils.toInputAction
 
 
 @Composable
@@ -89,7 +90,7 @@ fun PlayerScoreDisplay(
             color = Color.Black,
             shape = RoundedCornerShape(321.dp)
         )
-        //.background(Color.Black)
+    //.background(Color.Black)
 ) {
     Box(
         modifier = Modifier
@@ -195,7 +196,7 @@ fun main() = application {
     Window(
         state = windowState,
         onCloseRequest = ::exitApplication,
-        onKeyEvent = { keyEvent -> userScoreInput.applyKeyEvent(keyEvent) },
+        onKeyEvent = { keyEvent -> userScoreInput.applyInputAction(keyEvent.toInputAction()) },
     ) {
         GameWindow(game, userScoreInput)
     }
