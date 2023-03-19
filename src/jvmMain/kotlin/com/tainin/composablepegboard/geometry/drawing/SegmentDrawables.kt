5ef8be19d715f92548ff23864fbe231c39beaa56
Jakob Schechter <jakeeli52@gmail.com>
2023-03-19 17:30:58 -0400

    Added SegmentDrawables.kt as base for new drawing system

---
+++ src/jvmMain/kotlin/com/tainin/composablepegboard/geometry/drawing/SegmentDrawables.kt
@@ -0,0 +1,56 @@
+package com.tainin.composablepegboard.geometry.drawing
+
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Fill
+import com.tainin.composablepegboard.model.Player
+
+abstract class SegmentDrawable {
+    abstract fun DrawScope.draw()
+}
+
+abstract class HighlightedSegmentDrawable(
+    val usePlayerHighlights: Boolean,
+) : SegmentDrawable() {
+    protected abstract fun DrawScope.drawHighlight()
+    protected abstract fun DrawScope.drawOverHighlight()
+
+    final override fun DrawScope.draw() {
+        if (usePlayerHighlights)
+            drawHighlight()
+        drawOverHighlight()
+    }
+}
+
+abstract class ScoringSegmentDrawable(
+    val player: Player,
+    val lineThickness: Float,
+    usePlayerHighlight: Boolean,
+) : HighlightedSegmentDrawable(usePlayerHighlight) {
+    protected abstract fun getHoleOffset(fraction: Float): Offset
+    fun getHoleOffset(index: Int) = getHoleOffset((0.5f + index) / 5f)
+    final override fun DrawScope.drawOverHighlight() =
+        (0 until 5).forEach { i ->
+            val center = getHoleOffset(i)
+            drawCircle(
+                color = Color.Black,
+                radius = lineThickness / 2f,
+                center = center,
+                style = Fill,
+            )
+        }
+}
+
+class MultiSegmentDrawable<Drawable : SegmentDrawable>(
+    private val drawables: Sequence<Drawable>,
+) : SegmentDrawable() {
+    override fun DrawScope.draw() =
+        drawables.forEach { drawable ->
+            with(drawable) { draw() }
+        }
+}
+
+typealias MultiScoringSegmentDrawable = MultiSegmentDrawable<ScoringSegmentDrawable>
+
+fun DrawScope.drawSegment(drawable: SegmentDrawable) = with(drawable) { draw() }
