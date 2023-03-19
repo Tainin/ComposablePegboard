138e3129e943b2fc0a683f4de4c28d4bb728e651
Jakob Schechter <jakeeli52@gmail.com>
2023-03-19 17:46:52 -0400

    Added getDrawable implementation for ArcSegment

--- src/jvmMain/kotlin/com/tainin/composablepegboard/geometry/segments/ArcSegment.kt
+++ src/jvmMain/kotlin/com/tainin/composablepegboard/geometry/segments/ArcSegment.kt
@@ -1,8 +1,14 @@
 package com.tainin.composablepegboard.geometry.segments
 
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Rect
+import androidx.compose.ui.graphics.drawscope.DrawScope
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.unit.*
-import com.tainin.composablepegboard.geometry.drawing.MultiScoringSegmentDrawable
+import com.tainin.composablepegboard.geometry.drawing.MultiSegmentDrawable
+import com.tainin.composablepegboard.geometry.drawing.ScoringSegmentDrawable
 import com.tainin.composablepegboard.geometry.drawing.SegmentDrawingOptions
+import com.tainin.composablepegboard.model.Player
 import com.tainin.composablepegboard.utils.*
 import kotlin.math.round
 import kotlin.math.withSign
@@ -46,9 +52,76 @@ class ArcSegment(
 
     override fun getDrawable(
         segmentDrawingOptions: SegmentDrawingOptions,
-        density: Density,
-    ): MultiScoringSegmentDrawable {
-        TODO("Not yet implemented")
+        density: Density
+    ): MultiSegmentDrawable<ScoringSegmentDrawable> =
+        run {
+            val (baseRadius, halfWidth, lineThickness) =
+                with(density) {
+                    Triple(
+                        radius.toPx(),
+                        segmentDrawingOptions.run { streetWidth / 2f }.toPx(),
+                        segmentDrawingOptions.lineThickness.toPx(),
+                    )
+                }
+
+            val radiiBounds = Bounds(
+                start = baseRadius - halfWidth,
+                end = baseRadius + halfWidth,
+                reversed = angles.run { end > start }
+            )
+
+            val center = center.toOffset(density)
+
+            segmentDrawingOptions.players.mapIndexed { i, player ->
+                val (fStart, fStep) = segmentDrawingOptions.calcLineSpacingStartStep()
+                val lineRadius = radiiBounds.run {
+                    val f = fStart + (fStep * i)
+                    ((1 - f) * start) + (f * end)
+                }
+                Drawable(
+                    player = player,
+                    lineThickness = lineThickness,
+                    usePlayerHighlight = segmentDrawingOptions.useHighlight,
+                    center = center,
+                    radius = lineRadius,
+                    startAngle = arcAngles.start,
+                    sweepAngle = arcAngles.run { end - start },
+                )
+            }.let { drawables -> MultiSegmentDrawable(drawables) }
+        }
+
+    private class Drawable(
+        player: Player,
+        lineThickness: Float,
+        usePlayerHighlight: Boolean,
+        val center: Offset,
+        val radius: Float,
+        val startAngle: Float,
+        val sweepAngle: Float,
+    ) : ScoringSegmentDrawable(player, lineThickness, usePlayerHighlight) {
+
+        val rect = Rect(
+            left = center.x - radius,
+            top = center.y - radius,
+            right = center.x + radius,
+            bottom = center.y + radius,
+        )
+
+        val stroke = Stroke(lineThickness)
+
+        override fun DrawScope.drawHighlight() =
+            drawArc(
+                color = player.color.highlightColor,
+                startAngle = startAngle.radiansToDegrees(),
+                sweepAngle = sweepAngle.radiansToDegrees(),
+                useCenter = false,
+                topLeft = rect.topLeft,
+                size = rect.size,
+                style = stroke,
+            )
+
+        override fun getHoleOffset(fraction: Float) =
+            center + Offset.polarOffset(startAngle + (fraction * sweepAngle), radius)
     }
 }
 
